c1329419f57291a64b4fddb3a1a8854950c02f28
Devika Mohan <137169562+devikamohann@users.noreply.github.com>
2024-02-21 23:15:38 +0530

    Update yolox_voc_s.py

--- exps/example/yolox_voc/yolox_voc_s.py
+++ exps/example/yolox_voc/yolox_voc_s.py
@@ -9,7 +9,7 @@ class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
         self.num_classes = 11
-        self.max_epoch= 60
+        self.max_epoch= 100
         self.depth = 0.33
         self.width = 0.50
         self.warmup_epochs = 1
